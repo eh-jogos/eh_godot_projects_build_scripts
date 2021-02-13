56205bcddf5dc0f1d69af9de5889824e28e669b5
Daniel Queiroz Porto <danielvandalk@gmail.com>
2021-02-13 12:41:42 -0300

    Initial commit, trying to generalize it and run on bash instead of zsh

---
+++ build_standalone_releases.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# enters the folder this script is in
+cd "$(dirname "$0")"
+# go to previous folder, which is expected to be the godot project folder
+cd ..
+
+version=$1
+profile=$2
+filename=$3
+include_debug=$4
+
+# Fill the path to Godot binary you're using for this project, 
+# or the command you use to open godot from terminal
+godot_path=""
+
+# Fill in your project name, the same way you want the exported folder to be named. 
+# The folder name will be project name folowed by profile name. ex. CoolProjectWindows64
+project_name=""
+
+
+echo
+echo "Building Standalone release for: $profile"
+echo "Game Version is: $version"
+
+project_path=$(pwd)
+base_builds_path="$(dirname $project_path)/GameBuilds"
+
+echo "###########################################################"
+unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+final_path_release=$base_builds_path/$version/Release/$project_name$unquoted_profile/
+final_path_debug=$base_builds_path/$version/Debug/$project_name$unquoted_profile/
+
+echo "Exporting $unquoted_profile Release to $final_path_release"
+mkdir -p $final_path_release
+$godot_path --export $unquoted_profile $final_path_release$filename
+
+if [[ $include_debug = "true" ]]
+then
+	echo "Exporting $unquoted_profile Debug to $final_path_debug"
+	mkdir -p $final_path_debug
+	$godot_path --export-debug $unquoted_profile $final_path_debug$filename
+fi
+
+echo "Exporting $unquoted_profile Finished"
+echo "###########################################################"
+echo
